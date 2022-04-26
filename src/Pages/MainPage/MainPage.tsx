8c01e1451d94d8d8027863f23f97e03888df44e2
Alexander Rozhkov <rozhkov2222@icloud.com>
2022-04-27 00:12:49 +1000

    feature: добавлять желания в список

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,13 +1,44 @@
-import React, { FC } from 'react';
+import React, {
+  ChangeEvent, FC, FormEvent, useState,
+} from 'react';
 import './MainPage.scss';
 import Container from '../../components/common/Container/Container';
 
-const MainPage: FC = () => (
-  <main className="main-page">
-    <Container>
-      hello
-    </Container>
-  </main>
-);
+const MainPage: FC = () => {
+  const [value, setValue] = useState<string>('');
+  const [wishList, setWishList] = useState<string[] | []>([]);
+
+  const handleChangeValue = (e: ChangeEvent<HTMLInputElement>): void => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
+
+    setValue('');
+    setWishList([...wishList, ' ', value]);
+  };
+
+  return (
+    <main className="main-page">
+      <Container>
+        { wishList }
+
+        <form
+          action="#"
+          className="main-page__form"
+          onSubmit={handleSubmit}
+        >
+
+          <input
+            type="text"
+            value={value}
+            onChange={handleChangeValue}
+          />
+        </form>
+      </Container>
+    </main>
+  );
+};
 
 export default MainPage;
